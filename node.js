a9c07dbfeae5a1bf420d110a43d664b3ae3f745e
RaindropsSys <contact@minteck.org>
2023-04-23 10:52:53 +0200

    Updated node.js and added 4 files (automated)

--- node.js
+++ node.js
@@ -367,7 +367,15 @@ async function web() {
     let pings = JSON.parse(fs.readFileSync("./pings.json").toString());
 
     let rendered = ejs.render(fs.readFileSync("./web/page.ejs").toString(), { config, services: output["servers"], ping: output["ping"], pings: [Object.keys(pings), Object.values(pings)], outage: config['outage'], maintenances: config['maintenances'], history: JSON.parse(fs.readFileSync("./history.json").toString()), date: new Date(output["date"]), servers: JSON.parse(fs.readFileSync("./servers.json").toString()) });
-    fs.writeFileSync("./web/public/index.html", rendered);
+    let rendered2 = ejs.render(fs.readFileSync("./web/page2.ejs").toString(), { config, services: output["servers"], ping: output["ping"], pings: [Object.keys(pings), Object.values(pings)], outage: config['outage'], maintenances: config['maintenances'], history: JSON.parse(fs.readFileSync("./history.json").toString()), date: new Date(output["date"]), servers: JSON.parse(fs.readFileSync("./servers.json").toString()) });
+
+    if (new Date().getTime() > 1682240400000) {
+        fs.writeFileSync("./web/public/index.html", rendered2);
+        fs.writeFileSync("./web/public/index2.html", rendered);
+    } else {
+        fs.writeFileSync("./web/public/index.html", rendered);
+        fs.writeFileSync("./web/public/index2.html", rendered2);
+    }
 
     for (let asset of fs.readdirSync("./web/static")) {
         fs.copyFileSync("./web/static/" + asset, "./web/public/" + asset);
