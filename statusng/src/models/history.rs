f034ac107994cbfd8a2e80d69041bc3fbe05395c
Ryze312 <50497128+ryze312@users.noreply.github.com>
2024-06-16 17:35:42 +0300

    Clean up History a bit

--- statusng/src/models/history.rs
+++ statusng/src/models/history.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::collections::HashMap;
 use std::time::SystemTime;
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, NaiveDate, Utc};
 use serde::{Deserialize, Serialize};
 
 use crate::error::StatusError;
@@ -10,49 +10,48 @@ use super::service::{Service, ServiceStatus};
 
 type ServiceHistory = HashMap<String, Vec<ServiceStatus>>;
 
-fn get_date() -> String {
-    let now = SystemTime::now();
-    let now: DateTime<Utc> = now.into();
-    let now = now.to_rfc3339();
-    now.split('T')
-        .next().unwrap_or("0000-00-00")
-        .to_string()
+fn get_current_date() -> String {
+    let now: DateTime<Utc> = SystemTime::now().into();
+    now.format("%Y-%m-%d").to_string()
+}
+
+fn should_keep_entry(current_date: NaiveDate, entry_key: &str) -> bool {
+    let date = NaiveDate::parse_from_str("%Y-%m-%d", entry_key)
+        .unwrap_or_default();
+    let difference = current_date - date;
+
+    difference.num_days() <= 90
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct History(pub HashMap<String, ServiceHistory>);
 
 impl History {
-    pub fn add_entry(&mut self, service: &Service, code: ServiceStatus) {
-        let hm_root = &mut self.0;
-
-        let key = if hm_root.contains_key(service.get_legacy_id()) {
-            service.get_legacy_id()
+    fn get_id_for_service(&self, service: &Service) -> String {
+        // First check for legacy key format
+        if self.0.contains_key(service.get_legacy_id()) {
+            service.get_legacy_id().to_owned()
         } else {
-            &service.get_unique_id()
-        };
+            service.get_unique_id()
+        }
+    }
 
-        let hm_service = hm_root.entry(key.to_string()).or_default();
-        let date = get_date();
+    pub fn add_entry(&mut self, service: &Service, code: ServiceStatus) {
+        let id = self.get_id_for_service(service);
 
-        let statuses = hm_service.entry(date).or_default();
+        let service = self.0.entry(id).or_default();
+        let date = get_current_date();
+
+        let statuses = service.entry(date).or_default();
         statuses.push(code);
     }
 
     pub fn vacuum(&mut self) {
-        let now = SystemTime::now();
-        let now: DateTime<Utc> = now.into();
+        let now: DateTime<Utc> = SystemTime::now().into();
         let now = now.date_naive();
 
-        for hm_service in &mut self.0.values_mut() {
-            hm_service.retain(|k, _| {
-                let date_time = DateTime::parse_from_rfc3339(&format!("{}T00:00:00Z", k))
-                    .unwrap_or_default().date_naive();
-                let difference = now - date_time;
-
-                difference.num_days() <= 90
-            });
-        }
+        self.0.values_mut()
+            .for_each(|service| service.retain(|date, _| should_keep_entry(now, date)));
     }
 
     pub fn sync(&self) -> Result<(), StatusError> {
