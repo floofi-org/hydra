bc87da8cdecbc1c1418e49c1317875985ee44519
The Starscouts <starscouts@equestria.dev>
2024-07-10 12:10:42 +0200

    Fix Vercel module assuming JSON as MIME type (#1)
    Merging in spite of lack of review.

--- statusng/src/export/vercel.rs
+++ statusng/src/export/vercel.rs
@@ -9,7 +9,7 @@ impl<'a> Vercel<'a> {
         Self { token }
     }
 
-    pub fn put(&self, data: &[u8], path: &str, max_age: u32) -> Result<(), StatusError> {
+    pub fn put(&self, data: &[u8], path: &str, max_age: u32, mime_type: &str) -> Result<(), StatusError> {
         let url = format!("https://blob.vercel-storage.com/{path}");
         let authorization = format!("Bearer {}", self.token);
 
@@ -17,7 +17,7 @@ impl<'a> Vercel<'a> {
             .set("Access", "public")
             .set("Authorization", &authorization)
             .set("X-API-Version", "7")
-            .set("X-Content-Type", "application/json")
+            .set("X-Content-Type", mime_type)
             .set("X-Cache-Control-Max-Age", &max_age.to_string())
             .set("X-Add-Random-Suffix", "0")
             .send_bytes(data)?;
